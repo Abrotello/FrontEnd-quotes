f50a9a4d81947896688b7569c1dac9e613a58ffa
Adrian Botello <adrianbotellog01@gmail.com>
2025-03-11 02:05:55 -0600

    update: optimized code

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,30 @@
+import { Card } from "./model/card.js";
+import { cardService } from "./service/cardService.js";
+
+const card = Card()
+
+const button = document.getElementById("add-card")
+
+if (!button) { 
+    alert("Error, intentar nuevamente") 
+} else {
+    const mainContainer = document.getElementById("main-container")
+    button.onclick = () => {
+        if(card.getCounter() < 5) {
+            const cardData = cardService()
+            const newCard = card.createCard( cardData )
+            mainContainer.appendChild(newCard)
+        } else {
+            alert("Ya no es posible generar mas ventanas")
+        }
+    }
+
+}
+
 let newX = 0, newY = 0, startX = 0, startY = 0
 
-const card = document.getElementById('card')
-card.addEventListener('mousedown', mouseDown)
+const cardElement = document.getElementById('card')
+cardElement.addEventListener('mousedown', mouseDown)
 
 function mouseDown( e ) {
     startX = e.clientX
@@ -18,10 +41,11 @@ function mouseMove( e ) {
     startX = e.clientX
     startY = e.clientY
 
-    card.style.top = ( card.offsetTop - newY ) + 'px'
-    card.style.left = ( card.offsetLeft - newX ) + 'px'
+    cardElement.style.top = ( cardElement.offsetTop - newY ) + 'px'
+    cardElement.style.left = ( cardElement.offsetLeft - newX ) + 'px'
 }
 
 function mouseUp( e ) {
     document.removeEventListener('mousemove', mouseMove)
 }
+
